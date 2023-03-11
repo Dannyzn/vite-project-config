70ede1fc9bdedf944d8383da21af55379417f7a4
Dannyzn <694518723@qq.com>
2023-03-11 16:39:46 +0800

    chore: 更新项目配置文件

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// 引入 path 包注意两点:
+// 1. 为避免类型报错，你需要通过 `pnpm i @types/node -D` 安装类型
+// 2. tsconfig.node.json 中设置 `allowSyntheticDefaultImports: true`，以允许下面的 default 导入方式
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 手动指定项目根目录位置
+  // root: path.resolve(__dirname, './'),
+  // root: path.join(__dirname, 'src')
   plugins: [react()],
 })

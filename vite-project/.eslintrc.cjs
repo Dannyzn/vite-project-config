b77c34b740576d9563c14456c375f1cbb2c5ad78
Dannyzn <694518723@qq.com>
2023-03-16 21:43:05 +0800

    chore: prettier---link--eslint

--- vite-project/.eslintrc.cjs
+++ vite-project/.eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    // 1. 接入 prettier 的规则
     'prettier',
     'plugin:prettier/recommended'
   ],
@@ -18,8 +19,10 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
+  // 2. 加入 prettier 的 eslint 插件
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
+    // 3. 注意要加上这一句，开启 prettier 自动修复的功能
     'prettier/prettier': 'error',
     quotes: ['error', 'single'],
     semi: ['error', 'always'],

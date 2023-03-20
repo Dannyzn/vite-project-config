9ec5301545f135af29faf705577a3f4038ca081e
Dannyzn <694518723@qq.com>
2023-03-20 22:14:19 +0800

    feat: vite 中如何加载 web-worker脚本

--- vite-project/src/components/Header/index.tsx
+++ vite-project/src/components/Header/index.tsx
@@ -1,7 +1,14 @@
 import styles from './index.module.scss';
 import logoSrc from '@assets/imgs/vite.png';
 import {ReactComponent as ReactLogo} from '@assets/icons/logo.svg';
+import Worker from './example.js?worker';
 
+// 1. 初始化 Worker 实例
+const worker = new Worker();
+// 2. 主线程监听 worker 的信息
+worker.addEventListener('message', (e) => {
+    console.log(e);
+});
 
 export function Header() {
   return (

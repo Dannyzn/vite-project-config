76807be1e470f44edd8f9ace60f79c7af2232e72
Dannyzn <694518723@qq.com>
2023-03-20 22:12:32 +0800

    feat: unknown

--- vite-project/src/components/Header/index.tsx
+++ vite-project/src/components/Header/index.tsx
@@ -1,5 +1,15 @@
 import styles from './index.module.scss';
+import logoSrc from '@assets/imgs/vite.png';
+import {ReactComponent as ReactLogo} from '@assets/icons/logo.svg';
+
 
 export function Header() {
-  return <p className={styles.header}>This is Header</p>
+  return (
+    <div className={`p-20px text-center ${styles.header}`}>
+      {/* <!-- 省略前面的组件内容 -->
+      <!-- 使用图片 --> */}
+      <img className="m-auto mb-4" src={logoSrc} alt="" />
+      <ReactLogo  style={{width: 100}}/>
+    </div>
+  )
 };
